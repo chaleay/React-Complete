44b6518437a331925c513c74ef10bb9af821ef2d
chaleay <elijahgaytan@utexas.edu>
2020-09-06 15:44:59 -0500

    started assignment1

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,37 +1,75 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'; //capital letter to identify custom componenet 
+import Person from './Person/Person'; //capital letter to identify custom component - thats the convention
+
 
 class App extends Component {
   //State is a reserved property name
   state = {
     persons: [
       { name: 'Max', age: 28 },
-      { name: 'Stephy', age: 27 }
+      { name: 'Stephy', age: 27 },
+      { name: 'Cuntmuffin', age: 21 }
 
     ]
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     this.setState({
       persons: [
+        { name: newName, age: 30},
         { name: 'FuckerFag', age: 28 },
-        { name: 'Stephy', age: 27 }
+        { name: 'JWall', age: 30 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 30},
+        //updates on every keystroke
+        { name: event.target.value, age: 28 },
+        { name: 'JWall', age: 30 }
       ]
     })
   }
 
   render() {
+    const style = {
+      backgroundColor: 'cream',
+      font: 'inherit',
+      border: '2px solid #4C9FD6',
+      padding: '8px',
+      cursor: 'pointer'
+      
+
+    };
+
+    //Crea
+
     return (
       //JSX
       //typical to root things in one single element (div)
+      //when using an arrow function, it implicity adds a return keyword in front of the code
+      //warning - the arrow notation can be inefficient
       <div className="App">
       <br></br>
-      <button onClick = {this.switchNameHandler}>Switch Name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies include being gay</Person>
-      <Person name="Bimmy" age="29"></Person>
+      <button 
+      style={style}
+      onClick = {() => this.switchNameHandler('Mr. Faggot#2')}>Switch Name</button>
+      <Person 
+      name={this.state.persons[0].name} 
+      age={this.state.persons[0].age}></Person>
+      <Person 
+      name={this.state.persons[1].name} 
+      age={this.state.persons[1].age}
+      click={this.switchNameHandler.bind(this, 'MisterTomy')}
+      changed={this.nameChangedHandler}>My hobbies include being gay</Person>
+      <Person 
+      name={this.state.persons[2].name} 
+      age={this.state.persons[2].age}></Person>
       </div>
     );
     
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     //return jsx
     //example
     //return <p>This is me, a person at {Math.floor(Math.random() * 30)} years of age</p>
-    return <p>My name is {props.name} and my age is {props.age}</p>
+    return (
+    <div className='Person'>
+        <p onClick={props.click}>My name is {props.name} and my age is {props.age} </p>
+        <p>{props.children}</p>
+        <input type = "text" onChange={props.changed} value = {props.name}></input>
+    </div>
+    )
 };
 
 export default person;
